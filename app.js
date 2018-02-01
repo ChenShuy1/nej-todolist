1cd2c86c61c83b63bd185cedf91806fa8b5463a5
chenshy39 <82263980@qq.com>
2018-02-01 12:11:26 +0800

    发送静态资源文件&修改路由问题

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var fs = require('fs');
 
 var app = express();
 var db = require('./db');
@@ -24,9 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 app.use(api);
+app.use('/', function(req, res) {
+  fs.readFile(path.resolve(__dirname, 'public/html/index.html'), function (err, file) {
+    res.writeHead('200', 'OK')
+    res.end(file)
+  })
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
